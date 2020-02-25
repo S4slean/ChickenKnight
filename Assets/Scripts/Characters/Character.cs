b9e59125eb99dca81682612ad1966e10ed0e89ee
Stanislas Dubuc <stanislas.dubuc@gmail.com>
2020-02-25 22:29:03 +0100

    Character Script

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	[Header("References")]
+	public Transform self;
+	public Rigidbody rb;
+
+	[Header("Movement Stats")]
+	public AnimationCurve acceleration;
+	public float maxSpeed;
+}
