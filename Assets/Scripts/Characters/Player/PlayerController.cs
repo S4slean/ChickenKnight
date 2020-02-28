5f8bc89de8a8c2bee71d40b9d3efa71505a36dbb
Stanislas Dubuc <stanislas.dubuc@gmail.com>
2020-02-29 00:01:46 +0100

    Character + Player Controller

---
+++ Assets/Scripts/Characters/Player/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Character
+{
+	protected override void Update()
+	{
+		MoveHorizontal(Input.GetAxis("Horizontal"));
+		if(Input.GetButtonDown("Jump")) Jump();
+
+		base.Update();
+	}
+}
